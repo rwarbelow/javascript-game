7e161c4a5cb920af66a006931d4069bc72172811
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-04-05 19:34:49 -0700

    added functions to select and unselect divs

--- application.js
+++ application.js
@@ -1,21 +1,42 @@
 $( document ).ready(function() {
 	$('.block').css('background-color', getRandomColor);
-	// $('.block').append('<h1>' + getRandomNumber() + '</h1>');
+	$('.block').data("color", getRandomColor())
 	$(".block").each(function(){
 		var randNumber = getRandomNumber();
-		$(this).append('<h1>' + getRandomNumber() + '</h1>');
+		$(this).append('<p class="large-text">' + randNumber + '</p>');
+	});
+
+	$('.block').click(function(){
+		if ($(this).hasClass('isSelected')) {
+			removeSelected($(this));
+		} 
+		else {
+			select($(this));
+		}
+
 	});
 });
+
 
 var numbers = [1,2,3,4,5];
 function getRandomNumber(){
 	return numbers[Math.floor(Math.random() * numbers.length)];
 }
 function getRandomColor() {
-	var letters = '0123456789ABCDEF'.split('');
+	var letters = '0123456'.split('');
 	var color = '#';
 	for (var i = 0; i < 6; i++ ) {
-		color += letters[Math.round(Math.random() * 15)];
+		color += letters[Math.round(Math.random() * 6)];
 	}
 	return color;
+}
+
+function removeSelected(item){
+	$(item).removeClass('isSelected');
+	$(item).css('background-color', getRandomColor());
+}
+
+function select(item) {
+	$(item).css('background-color',"")
+	$(item).addClass('isSelected');
 }

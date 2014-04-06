c7465083c4cade40188fff48159630bc4034816e
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-04-05 21:14:08 -0700

    game is not very fun, but it works

--- application.js
+++ application.js
@@ -9,26 +9,37 @@ $( document ).ready(function() {
 	$('.block').click(function(){
 		changeClass($(this));
 	});
+
 	$(document).keypress(function(e) {
 		if(e.which == 13) {
-			var selected = $('.isSelected').length;
-			if (selected == 2){
-				var toAdd = ($('.isSelected').text()).split('');
-				var total = 0;
-				jQuery.each( toAdd, function( i, val ) {
-					total += parseInt(val);
-				});
-				alert(total);
+			if ($('.isSelected').length == 2){
+				total = findTotal();
+				if (total == 100){
+					winningScreen();
+				};
+				reloadBoard(total);
 			}
 		}
 	});
-
 });
 
 var WINNING_NUMBER = 100;
-var numbers = [1,2,3,4,5];
+var numbers = [2,4,8,10,12,13,19];
+
+function findTotal(){
+	var toAdd = []
+	$('.isSelected').each(function(){
+		toAdd.push($(this).text())
+	});
+	var total = 0;
+	jQuery.each( toAdd, function( i, val ) {
+		total += parseInt(val);
+	});
+	return total;
+}
+
 function getRandomNumber(){
-	return numbers[Math.floor(Math.random() * numbers.length)];
+	return numbers[Math.floor(Math.random() * 7)];
 }
 function getRandomColor() {
 	var letters = '0123456'.split('');
@@ -57,3 +68,16 @@ function changeClass(item){
 		changeToSelected($(item));
 	}
 }
+
+function reloadBoard(total){
+	$('.isSelected').eq(1).html('<p class="large-text">' + total + '</p>');
+	$('.block:not(".isSelected")').each(function(){
+		$(this).html('<p class="large-text">' + getRandomNumber() + '</p>')
+	});
+	removeSelected($('.isSelected'));
+}
+
+function winningScreen(){
+	$('.container').html('<div style="text-align:center"><p class="won">You Won!</p></div>')
+};
+

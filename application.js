076fe0c4221bd8b1c1b40290d3791190f2e910dc
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-04-06 11:34:25 -0700

    changed game to require three 50 blocks to win

--- application.js
+++ application.js
@@ -1,8 +1,7 @@
 $( document ).ready(function() {
 	setGameBoard();
-	$('.block').click(function(){
-		changeClass($(this));
-		playGame();
+	$('.active').click(function(){
+		playGame(this);
 	});
 	$(".container").on('click', '.play-again', function() {
 		location.reload();
@@ -11,42 +10,62 @@ $( document ).ready(function() {
 
 
 var multiply_by = [-1,1]
-var clicks = 10
+var clicks = 30
+var fifties = 0
 
-function checkLoss(){
-	if (clicks == 1){
+function checkLoss(total){
+	if (clicks == 0 && total != 50){
 		losingScreen();
 	};
 };
 
+function checkWin(){
+	if (fifties == 3){
+		winningScreen();
+	}
+};
+
 function setGameBoard(){
 	$('.score').html('<p class="score-heading">' + clicks + '</p>');
-	$('.block').each(function(){
+	$('.active').each(function(){
 		$(this).append('<p class="large-text">' + getRandomNumber() + '</p>');
-		$(this).css('background-color', getRandomColor())
+		$(this).css('background-color', '#3498db')
 	});
 };
 
-function playGame(){
+function playGame(block){
+	changeClass(block);
 	if ($('.isSelected').length == 2){
-		checkLoss();
-		clicks -= 1;
 		updateScore();
 		total = findTotal();
-		checkWin(total);
+		checkFifties(total);
 		reloadBoard(total);
 	}
 };
 
-function checkWin(total){
+function checkFifties(total){
+	checkLoss(total);
 	if (total == 50){
-		winningScreen();
+		fifties += 1
+		lockTile();
+		checkWin();
 	};
 };
 
-function updateScore(){
-	$('.score').html('<p class="score-heading">' + clicks + '</p>');
+function lockTile(){
+	selected = $('.isSelected').eq(1)
+	selected.html('<p class="large-text">50<br><img style="width:18px" src="star.png"></p>');
+	selected.removeClass('active isSelected');
+	selected.addClass('lock');
 };
+
+function reloadBoard(total){
+	$('.isSelected:not(.lock)').eq(1).html('<p class="large-text">' + total + '</p>');
+	$('.block:not(.isSelected, .lock)').each(function(){
+		$(this).html('<p class="large-text">' + getRandomNumber() + '</p>')
+	});
+	removeSelected($('.isSelected'));
+}
 
 function findTotal(){
 	var toAdd = []
@@ -60,21 +79,29 @@ function findTotal(){
 	return total;
 }
 
+function updateScore(){
+	clicks -= 1;
+	$('.score').html('<p class="score-heading">' + clicks + '</p>');
+};
+
+function changeClass(item){
+	if ($(item).hasClass('isSelected')) {
+		$(item).removeClass('isSelected');
+		$(item).css('background-color', '#3498db');
+	}
+	else if ($(item).hasClass('active')) {
+		$(item).css('background-color',"")
+		$(item).addClass('isSelected');
+	}
+}
+
 function getRandomNumber(){
 	return (multiply_by[Math.round(Math.random())] * Math.floor(Math.random() * 20) + 1);
-}
-function getRandomColor() {
-	var letters = '0123456'.split('');
-	var color = '#';
-	for (var i = 0; i < 6; i++ ) {
-		color += letters[Math.round(Math.random() * 6)];
-	}
-	return color;
 }
 
 function removeSelected(item){
 	$(item).removeClass('isSelected');
-	$(item).css('background-color', getRandomColor());
+	$(item).css('background-color', '#3498db');
 }
 
 function changeToSelected(item) {
@@ -82,29 +109,12 @@ function changeToSelected(item) {
 	$(item).addClass('isSelected');
 }
 
-function changeClass(item){
-	if ($(item).hasClass('isSelected')) {
-		removeSelected($(item));
-	} 
-	else {
-		changeToSelected($(item));
-	}
-}
-
-function reloadBoard(total){
-	$('.isSelected').eq(1).html('<p class="large-text">' + total + '</p>');
-	$('.block:not(".isSelected")').each(function(){
-		$(this).html('<p class="large-text">' + getRandomNumber() + '</p>')
-	});
-	removeSelected($('.isSelected'));
-}
-
 function winningScreen(){
-	$('.container').html('<div style="text-align:center"><p class="won">You Won!</p></div><div class="play-again">Play again</div>')
+	$('.container').html('<div style="text-align:center;margin: 0 auto"><p class="won">You Won!</p></div><div class="play-again"><img style="width:55px" src="star.png"><br><br>Play again</div>')
 };
 
 function losingScreen(){
-	$('.container').html('<div style="text-align:center"><p class="lost">You lost :(</p></div><div class="play-again">Play again</div>')
+	$('.container').html('<div style="text-align:center;"><div style="text-align:center;margin: 0 auto"><p class="lost">You lost :(</p><div class="play-again">Play again</div></div></div>')
 };
 
 
